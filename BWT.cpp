a980d051354dd133e7c301efa069c9f15e4b2a7a
StrongFlaky <strongli2014@hotmail.com>
2020-05-12 22:58:44 +0000

    ss

--- BWT.cpp
+++ BWT.cpp
@@ -1,10 +1,23 @@
 #include <string>
+#include <algorithm>
 #include "BWT.h"
+#include <vector>
 using namespace std;
 
 /**
  * Implement bwt() correctly
  */
 string bwt(const string & s) {
-    /* YOUR CODE HERE */
+    string btwFinal;
+    vector<string> bwtBegin;
+    int length = s.size();
+    for(int i = 0; i < length; i++){
+        string currentRotation = s.substr(i, length) + s.substr(0, i);
+        bwtBegin.push_back(currentRotation);
+    }
+    sort(bwtBegin.begin(), bwtBegin.end());
+    for(int i = 0; i < length; i++){
+        btwFinal = btwFinal + bwtBegin[i].substr(length-1);
+    }
+    return btwFinal;
 }
--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -1,11 +1,28 @@
 #include <string>
 #include <vector>
 #include "SuffixArray.h"
+#include <unordered_map> 
+#include <algorithm>
 using namespace std;
 
 /**
  * Implement suffix_array() correctly
  */
 vector<unsigned int> suffix_array(const string & s) {
-    /* YOUR CODE HERE */
+    
+    vector<string> arrayKey;
+    vector<unsigned int> arrayMap;
+    int length = s.size();
+    for(int i = 0; i < length; i++){
+        arrayKey.push_back(s.substr(i, length));
+        arrayMap.push_back(i);
+    }
+    auto compare = [&](int previous, int next){
+        if(arrayKey.at(previous) > arrayKey.at(next)){
+            return false;
+        }
+        return true;
+    };
+    std::sort(arrayMap.begin(), arrayMap.end(), compare);
+    return arrayMap;
 }
